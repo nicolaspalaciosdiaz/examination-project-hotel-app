1012925e6df9b37bd4c83fab149bf238c7889b70
Nicolas <nicolas88palacios@gmail.com>
2022-01-26 19:17:29 +0100

    #5 Added application.properties file to connect to heroku database

--- src/main/java/com/example/HotelApp/controller/DummyController.java
+++ src/main/java/com/example/HotelApp/controller/DummyController.java
@@ -1,2 +1,13 @@
-package com.example.HotelApp.controller;public class DummyController {
+package com.example.HotelApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyController {
+
+    @GetMapping("/dummy")
+    public String dummy() {
+        return "Dummy string";
+    }
 }
