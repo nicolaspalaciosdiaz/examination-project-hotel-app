9599cc25ed13fae4dd30a5198137efec3ec3dfc6
Nicolas <nicolas88palacios@gmail.com>
2022-03-16 07:29:52 +0100

    #48 #49 #50 Created api for Service Provided

---
+++ src/main/java/com/example/HotelApp/controller/ServiceProvidedController.java
@@ -0,0 +1,55 @@
+package com.example.HotelApp.controller;
+
+import com.example.HotelApp.model.Booking;
+import com.example.HotelApp.model.ServiceProvided;
+import com.example.HotelApp.repository.ServiceProvidedRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class ServiceProvidedController {
+
+    @Autowired
+    ServiceProvidedRepository serviceProvidedRepository;
+
+    @GetMapping("/serviceprovided")
+    public ResponseEntity<List<ServiceProvided>> getAllServiceProvided(){
+        return new ResponseEntity<>(serviceProvidedRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/serviceprovided/{id}")
+    public ResponseEntity<ServiceProvided> getServiceProvidedById(@PathVariable("id") long id) {
+        Optional<ServiceProvided> serviceProvided = serviceProvidedRepository.findById(id);
+        if (serviceProvided.isPresent()) {
+            return new ResponseEntity<>(serviceProvided.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/serviceprovided/createserviceprovided")
+    public ResponseEntity<ServiceProvided> createServiceProvided(@RequestBody ServiceProvided serviceProvided) {
+        try {
+            serviceProvidedRepository.save(serviceProvided);
+            return new ResponseEntity<>(serviceProvided, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/serviceprovided/{id}")
+    public ResponseEntity<HttpStatus> deleteServiceProvided(@PathVariable("id") long id) {
+        try {
+            serviceProvidedRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.GONE);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/com/example/HotelApp/repository/ServiceProvidedRepository.java
@@ -0,0 +1,7 @@
+package com.example.HotelApp.repository;
+
+import com.example.HotelApp.model.ServiceProvided;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceProvidedRepository extends JpaRepository<ServiceProvided, Long> {
+}
